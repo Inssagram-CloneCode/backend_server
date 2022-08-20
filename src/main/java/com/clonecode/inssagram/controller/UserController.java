dd5d790e4a003315b995a36cdcc7a37f10777b6e
Moveuk <v_donguk@naver.com>
2022-08-20 23:06:32 +0900

    [#3]Fix: UserController 스웨거 API 명세 설명 수정

--- src/main/java/com/clonecode/inssagram/controller/UserController.java
+++ src/main/java/com/clonecode/inssagram/controller/UserController.java
@@ -2,6 +2,8 @@ package com.clonecode.inssagram.controller;
 
 import com.clonecode.inssagram.dto.request.LoginRequestDto;
 import com.clonecode.inssagram.dto.request.SignUpRequestDto;
+import com.clonecode.inssagram.dto.response.LoginResponseDto;
+import com.clonecode.inssagram.dto.response.ResponseDto;
 import com.clonecode.inssagram.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -17,7 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-@Api(value = "유저 컨트롤러")
+@Api(tags = {"유저 API Controller"})
 @RequiredArgsConstructor
 @RestController
 public class UserController {
@@ -25,13 +27,13 @@ public class UserController {
     private final UserService userService;
 
     @RequestMapping(value = "/api/signup", method = RequestMethod.POST)
-    @ApiOperation(value = "회원가입", notes = "회원가입 기능")
+    @ApiOperation(value = "회원가입", notes = "회원가입 기능", response = ResponseDto.class)
     public ResponseEntity signup(@RequestBody @Valid SignUpRequestDto requestDto) {
         return new ResponseEntity<>(userService.createUser(requestDto), HttpStatus.valueOf(HttpStatus.OK.value()));
     }
 
     @RequestMapping(value = "/api/login", method = RequestMethod.POST)
-    @ApiOperation(value = "로그인", notes = "로그인 기능")
+    @ApiOperation(value = "로그인", notes = "로그인 기능", response = LoginResponseDto.class)
     public ResponseEntity login(@RequestBody @Valid LoginRequestDto requestDto,
                                 HttpServletResponse response
     ) {
@@ -39,7 +41,7 @@ public class UserController {
     }
 
     @RequestMapping(value = "/api/auth/logout", method = RequestMethod.POST)
-    @ApiOperation(value = "로그아웃", notes = "로그아웃 기능")
+    @ApiOperation(value = "로그아웃", notes = "로그아웃 기능", response = ResponseDto.class)
     public ResponseEntity logout(HttpServletRequest request) {
         return new ResponseEntity<>("로그아웃", HttpStatus.valueOf(HttpStatus.OK.value()));
     }
