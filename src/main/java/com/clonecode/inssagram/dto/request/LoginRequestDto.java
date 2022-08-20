8bf5efb73d37f3d2fc0450c59e919bfa9d336bce
Moveuk <v_donguk@naver.com>
2022-08-20 23:07:41 +0900

    [#3]Feat: LoginRequestDto 스웨거 API 명세 설명 설정

--- src/main/java/com/clonecode/inssagram/dto/request/LoginRequestDto.java
+++ src/main/java/com/clonecode/inssagram/dto/request/LoginRequestDto.java
@@ -1,5 +1,6 @@
 package com.clonecode.inssagram.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,11 @@ import javax.validation.constraints.NotBlank;
 public class LoginRequestDto {
 
     @NotBlank
+    @ApiModelProperty(example = "사용자 이메일")
     private String email;
 
     @NotBlank
+    @ApiModelProperty(example = "사용자 비밀번호")
     private String password;
 
 }
