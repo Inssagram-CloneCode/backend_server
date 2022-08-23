5fdee32e99d67a57f53f728bdca5d15d97e36dc7
jeanniejang <jhjang.sdev@gmail.com>
2022-08-23 23:06:22 +0900

    [#1] fix: PostDetailResponseDto의 필드 commentDto로 수정 -comment entity를 보내주던 걸 dto로 변경

---
+++ src/main/java/com/clonecode/inssagram/dto/response/CommentResponseDto.java
@@ -0,0 +1,15 @@
+package com.clonecode.inssagram.dto.response;
+
+import com.clonecode.inssagram.domain.Comment;
+
+public class CommentResponseDto {
+    private UserProfileResponseDto user;
+    private Long commentId;
+    private String commentContents;
+
+    public CommentResponseDto (Comment comment){
+        this.user = new UserProfileResponseDto(comment.getUser());
+        this.commentId = comment.getId();
+        this.commentContents = comment.getCommentContents();
+    }
+}
