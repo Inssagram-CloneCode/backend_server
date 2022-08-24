29a0001836922ad2b1be41062b649b76daedc50c
HASEUNGHEEE <ariul123@gmail.com>
2022-08-24 15:50:33 +0900

    [#28] Feat: Heart 관련 파일 스웨거 설정

--- src/main/java/com/clonecode/inssagram/dto/request/HeartRequestDto.java
+++ src/main/java/com/clonecode/inssagram/dto/request/HeartRequestDto.java
@@ -1,8 +1,10 @@
 package com.clonecode.inssagram.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 @Getter
 public class HeartRequestDto {
+    @ApiModelProperty(example = "현재 좋아요 상태")
     private Long isHeart;
 }
